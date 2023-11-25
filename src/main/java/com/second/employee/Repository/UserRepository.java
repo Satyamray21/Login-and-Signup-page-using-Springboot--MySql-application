663f5d4367c3e75edf0eec73b481e8eb9b6aea97
Satyamray21 <satyamray43@gmail.com>
2023-11-25 22:55:30 +0530

    first commit

---
+++ src/main/java/com/second/employee/Repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.second.employee.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.second.employee.Model.UserModel;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Integer> {
+    Optional<UserModel>findByUsernameandPassword(String username,String password);
+
+}
