a9efd920beb66fea6177ead87fde677ce2d0ab13
Satyamray21 <satyamray43@gmail.com>
2023-11-26 10:36:13 +0530

    UserService added.

---
+++ src/main/java/com/second/employee/Service/UserService.java
@@ -0,0 +1,40 @@
+package com.second.employee.Service;
+
+import org.springframework.stereotype.Service;
+
+import com.second.employee.Model.UserModel;
+import com.second.employee.Repository.UserRepository;
+
+@Service
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public UserModel registerUser(String username,String fname,String sname,String email,String password,String phone){
+        if(username==null || password==null)
+        {
+            return null;
+        }else
+        {
+        UserModel userModel= new UserModel();
+        userModel.setEmail(email);
+        userModel.setFname(fname);
+        userModel.setPassword(password);
+        userModel.setUsername(username);
+        userModel.setSname(sname);
+        userModel.setPhone(phone);
+        return userRepository.save(userModel);
+        }
+        
+
+
+    }
+    public UserModel authenticate(String username,String password)
+    {
+        return userRepository.findByUsernameandPassword(username,password).orElse(null);
+
+    }
+}
